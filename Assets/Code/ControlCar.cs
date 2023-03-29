edb1c9a4460d9facd7d2cc8f577bd9497673c55a
axe-rigged <alex20101@student.hamk.fi>
2023-03-29 18:27:25 +0300

    Car movement acceptable. Handbreak not working.

--- Assets/Code/ControlCar.cs
+++ Assets/Code/ControlCar.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ControlCar : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Vector3 normal_mass_point = new Vector3(0f, 0f,0f);
+    public Vector3 drift_mass_point = new Vector3(0f, 0f, 0f);
+
     public WheelCollider front_r;
     public WheelCollider front_l;
     public WheelCollider back_r;
@@ -16,22 +20,28 @@ public class ControlCar : MonoBehaviour
 
     public float accelration = 500f;
     public float breakingforce = 300f;
+    public float drift_break = 400f;
 
     private float currentAcceleration = 0f;
     private float currentBreackForce = 0f;
     private float c_turn_angle = 0f;
     public float max_turn_angle = 20f;
 
-    // Start is called before the first frame update
+    private void Start(){
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = normal_mass_point;
+    }
+    //Fixed update mainly used
     private void FixedUpdate(){
         currentAcceleration = accelration * Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.Space)){
+        if (Input.GetKey(KeyCode.R)){
             currentBreackForce = breakingforce;
         }
         else {
             currentBreackForce = 0f;
         }
+
 
         front_l.motorTorque = currentAcceleration;
         front_r.motorTorque = currentAcceleration;
@@ -40,6 +50,7 @@ public class ControlCar : MonoBehaviour
         front_r.brakeTorque = currentBreackForce;
         back_l.brakeTorque = currentBreackForce;
         back_r.brakeTorque = currentBreackForce;
+        handbreak(Input.GetKey(KeyCode.Space));
 
         //get change on turning wheel
         c_turn_angle = max_turn_angle * Input.GetAxis("Horizontal");
@@ -50,6 +61,7 @@ public class ControlCar : MonoBehaviour
         turn_wheel_mesh(mesh_back_r, back_r, false);
         turn_wheel_mesh(mesh_front_l, front_l, true);
         turn_wheel_mesh(mesh_front_r, front_r, false);
+
     }
 
     //fucntion for turning wheels and rotating them
@@ -65,6 +77,25 @@ public class ControlCar : MonoBehaviour
         else {
             mesh.rotation = rotation * Quaternion.Euler(0,180,0);
             mesh.position = position;
+        }
+    }
+
+    private void handbreak(bool x){
+        if(x){
+            //do we need to close back wheels or what
+            //maybe give more force to move
+            rb.centerOfMass = drift_mass_point; 
+            rb.drag = 0f;
+            if(Input.GetKey(KeyCode.A)){
+                rb.AddRelativeTorque(0f,1200f,0f);
+            }
+            if(Input.GetKey(KeyCode.D)){
+                rb.AddRelativeTorque(0f, 1200f,0f);
+            }
+
+        } else {
+            rb.centerOfMass = normal_mass_point; 
+            rb.drag = 0.1f;
         }
     }
 }
---
+++ Assets/Code/camera_control.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_control : MonoBehaviour
+{
+    public Camera player_camera;
+    public GameObject player_car;
+    private Transform camera_position;
+    public Vector3 cameras_rest_position = new Vector3(0f, 3f, -5f);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //should find Tag for player car object
+        //Not sure if want to attach this script camera controller or to player and find camera with tag but now its here
+        camera_position = player_camera.transform;
+    }
+
+    void FixedUpdate()
+    {
+        camera_position.rotation = Quaternion.Euler(15f, player_car.transform.eulerAngles.y, 0f);
+        //Forgot how global and local space works in position
+        var move_position = player_car.transform.TransformPoint(cameras_rest_position);
+        //slerping
+        //camera_position.position = move_position; 
+        camera_position.position = Vector3.Lerp(camera_position.position, move_position, .3f);
+    }
+}
